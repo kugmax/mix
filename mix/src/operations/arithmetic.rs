7b8ec9a63a4366e9d57f38718f7d18a6bd3b19e4
Maksym Kuhatov <maximkugatov@gmail.com>
2023-01-13 20:16:30 +0200

    feat: added ADD

--- mix/src/operations/arithmetic.rs
+++ mix/src/operations/arithmetic.rs
@@ -53,6 +53,51 @@ impl ADD {
     }
 }
 
+struct SUB {
+    code: u32,
+    execution_time: u32,
+
+    instruction: Word,
+}
+
+impl SUB {
+    pub fn new(instruction: Word) -> SUB {
+        SUB {
+            code: 2,
+            execution_time: 2,
+            instruction: instruction,
+        }
+    }
+
+    pub fn execute(&self, mem: &Memory, reg: &mut Registers) {
+        let addr = self.instruction.get_address();
+        let addr = addr.abs();
+
+        let mem_cell = mem.get(addr as usize);
+
+        let value: i32 =
+            Word::new(mem_cell.get_by_access(self.instruction.get_f())).get_signed_value();
+        let result: i32 = reg.get_a().get_signed_value() - value;
+
+        if result == 0 {
+            // reg.set_overflow(false); //TODO: how to clean overflow flag ?
+            let mut result = Word::new(0);
+            result.set_sign(reg.get_a().get_sign());
+            reg.set_a(result);
+            return;
+        }
+
+        if result >= -MAX_5_BYTES && result <= MAX_5_BYTES {
+            // reg.set_overflow(false); //TODO: how to clean overflow flag ?
+            reg.set_a(Word::new_from_signed(result));
+            return;
+        }
+
+        reg.set_overflow(true);
+        reg.set_a(Word::new(0));//TODO: the behaviour have to be different
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -166,5 +211,44 @@ mod tests {
         operation.execute(&m, &mut r);
         assert_eq!(r.is_overflow(), true);
         assert_eq!(r.get_a(), Word::new(0));
+    }
+    
+    #[test]
+    fn sub() {
+        let mut m = Memory::new();
+
+        let mem_value = Word::new(1_001);
+        m.set(2_000, mem_value.get());
+
+        let mem_value = Word::new_from_signed(-1_001);
+        m.set(2_001, mem_value.get());
+
+        let mem_value = Word::new_from_signed(MAX_5_BYTES);
+        m.set(2_002, mem_value.get());
+
+        let mem_value = Word::new_from_signed(-MAX_5_BYTES);
+        m.set(2_003, mem_value.get());
+
+        let mut r = Registers::new();
+
+        let operation = SUB::new(Word::new_instruction(2_001, 0, WordAccess::new(0, 5), 1));
+        operation.execute(&m, &mut r);
+        assert_eq!(r.get_a(), Word::new(1_001));
+        assert_eq!(r.is_overflow(), false);
+
+        let operation = SUB::new(Word::new_instruction(2_000, 0, WordAccess::new(0, 5), 1));
+        operation.execute(&m, &mut r);
+        assert_eq!(r.get_a(), Word::new(0));
+        assert_eq!(r.is_overflow(), false);
+
+        let operation = SUB::new(Word::new_instruction(2_003, 0, WordAccess::new(0, 5), 1));
+        operation.execute(&m, &mut r);
+        assert_eq!(r.get_a(), Word::new_from_signed(MAX_5_BYTES));
+        assert_eq!(r.is_overflow(), false);
+
+        let operation = SUB::new(Word::new_instruction(2_002, 0, WordAccess::new(0, 5), 1));
+        operation.execute(&m, &mut r);
+        assert_eq!(r.get_a(), Word::new_from_signed(0));
+        assert_eq!(r.is_overflow(), false);
     }
 }
