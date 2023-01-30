4eb5ca0dc5dd04574fc63f7b68050b0af9735e35
Maksym Kuhatov <maximkugatov@gmail.com>
2023-01-30 13:05:21 +0200

    feat: OUT has first implementation

--- mix/src/operations/io.rs
+++ mix/src/operations/io.rs
@@ -3,23 +3,28 @@ use crate::memory::word::MAX_5_BYTES;
 use crate::memory::Bytes;
 use crate::memory::Instruction;
 use crate::memory::Memory;
+use crate::operations::conversion::*;
 use crate::operations::get_memory_cell;
 use crate::operations::*;
 use crate::registers::Registers;
 
+use std::fs::File;
+use std::io;
+use std::io::prelude::*;
 // pub const TAPE:IoUnit = IoUnit::UNIT(0, 100, "Tape unit number 0");
 //
 // pub enum IoUnit<'a> {
-  // UNIT(u8, u8, &'a str)
+// UNIT(u8, u8, &'a str)
 // }
 
 //TODO: create virual device which would write/read to the file
 //the name of the file containts unit type, info
-pub struct IO_UNIT {
-}
 
-impl IO_UNIT {
-}
+pub const IO_FILE_PREFIX: &str = "io_unit_";
+
+pub struct IO_UNIT {}
+
+impl IO_UNIT {}
 
 pub struct IN {
     code: u32,
@@ -35,6 +40,10 @@ impl IN {
 }
 impl Operation for IN {
     fn execute(&self, args: OperationArgs) -> OperationResult {
+        let io_unit = args.instruction.get_byte(4);
+        if io_unit != 18 {
+            panic!("unsupported io unit {io_unit}");
+        }
 
         OperationResult::from_args(self.execution_time, args)
     }
@@ -51,9 +60,42 @@ impl OUT {
             execution_time: 1, // + T
         }
     }
+
+    fn write(&self, io_unit: u8, words: Vec<Word>) -> io::Result<()> {
+        // let path = "/home/max/Documents/Projects.git/mix/mix/target/";
+        // let mut file = File::create(path.to_string() + &IO_FILE_PREFIX.to_string() + &io_unit.to_string())?;
+        //
+        let mut file = File::create(IO_FILE_PREFIX.to_string() + &io_unit.to_string())?;
+
+        let mut line = String::new();
+        for w in words {
+            for b in 1..6 {
+                line += &SYMBOLS[w.get_byte(b) as usize].to_string();
+            }
+        }
+
+        file.write_all(&line.as_bytes())?;
+
+        Ok(())
+    }
 }
 impl Operation for OUT {
     fn execute(&self, args: OperationArgs) -> OperationResult {
+        let io_unit = args.instruction.get_byte(4);
+        if io_unit != 18 {
+            panic!("unsupported io unit {io_unit}");
+        }
+        let unit_block = 24;
+        // let start_from = args.instruction.get_address();
+        let start_from = get_indexed_addr(args.instruction, args.reg);
+
+        let mut out_buffer = Vec::new();
+        for i in 0..unit_block {
+            let addr = (start_from + i) as usize;
+            out_buffer.push(args.mem.get(addr));
+        }
+
+        self.write(io_unit, out_buffer);
 
         OperationResult::from_args(self.execution_time, args)
     }
@@ -73,7 +115,28 @@ impl IOC {
 }
 impl Operation for IOC {
     fn execute(&self, args: OperationArgs) -> OperationResult {
-
         OperationResult::from_args(self.execution_time, args)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn out() {
+        let op = OUT::new();
+        let mut m = Memory::new();
+        let mut r = Registers::new();
+
+        // HELLO
+        //  WORL
+        //  D
+        m.set_bytes(0, 0, 8, 5, 13, 13, 16);
+        m.set_bytes(1, 0, 0, 26, 16, 19, 13);
+        m.set_bytes(2, 0, 4, 0, 0, 0, 0);
+
+        let args = OperationArgs::new(1, Word::new_by_bytes(0, &[0, 0, 0, 18, 37]), &mut m, &mut r);
+        op.execute(args);
     }
 }
