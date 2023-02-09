caa119043cefd16e4a4a783d7b90b221e11ad180
Maksym Kuhatov <maximkugatov@gmail.com>
2023-02-09 11:06:34 +0200

    feat: lexical structure

--- mixal/src/parser.rs
+++ mixal/src/parser.rs
@@ -8,16 +8,58 @@ use std::io::prelude::*;
 use std::io::BufReader;
 use std::str::FromStr;
 
+pub mod word;
+
+/*
+ * program -> lines
+ * lines -> lines line | e
+ * line -> loc op address
+ * 
+ * loc -> id | local_symbol | e 
+ * op -> mix_op | mixal_op
+ * address -> A_part||I_part||F_part | W_value 
+ *
+ * A_part -> expr | local_symbol | literal_constant | e
+ * I_part -> ,expr | e
+ * F_part -> (expr) | e
+ */
+
+/*
+ * literal_constant -> =W_value=
+ * W_value -> expr||F_part | ,W_value 
+ * //TODO: here recusion like ***...
+ * expr -> expr+atom_expr | expr-atom_expr | 
+ *         expr*atom_expr | expr+atom_expr |
+ *         expr/atom_expr | expr//atom_expr |
+ *         expr:atom_expr | 
+ *         unary
+ * unary -> -atom_expr | +atom_expr | atom_expr
+ * atom_expr -> num | id | *
+ */
+
+/*
+ * mix_op -> NOP | ... | CMPX
+ * mixal_op -> EQU | ORIG | CON | ALF | END
+ *
+ * local_symbol -> num||H | num||B | num||F
+ *
+ * id ->  chars | symbol 
+ * symbol -> chars||num | num||chars
+ * num -> 0|1|2|3|4|5|6|7|8|9
+ * chars -> A..Z
+ **/
+
+
 pub trait Printable {
     fn print(&self) -> String;
 }
 
-pub struct SymbolTable {
+pub struct DefinedSymbolTable {
     values: HashMap<String, i32>,
 }
-impl SymbolTable {
-    pub fn new() -> SymbolTable {
-        SymbolTable {
+impl DefinedSymbolTable {
+    pub fn new() -> DefinedSymbolTable {
+        DefinedSymbolTable {
             values: HashMap::new(),
         }
     }
@@ -27,8 +69,9 @@ impl SymbolTable {
             .get(name)
             .expect(&format!("symbol not found {name}")[..])
     }
+
     pub fn put(&mut self, name: &str, value: i32) {
-        self.put(name, value);
+        self.values.insert(name.to_string(), value);
     }
 }
 
@@ -42,16 +85,30 @@ impl ProgramLine {
         ProgramLine { loc, op, addr }
     }
 }
+impl fmt::Debug for ProgramLine {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "|{}|{}|{}|", self.loc, self.op, self.addr)
+    }
+}
 
-pub struct Parser {}
-impl Parser {
-    pub fn new() -> Parser {
-        Parser {}
+pub struct Parser<'a> {
+    defined_symbols: DefinedSymbolTable,
+    tags: Tags<'a>,
+}
+impl<'a> Parser<'a> {
+    pub fn new() -> Parser<'a> {
+        Parser {
+            defined_symbols: DefinedSymbolTable::new(),
+            tags: Tags::new(),
+        }
+    }
+    pub fn parse(&self, lines: Vec<String>) {
+        let pr_lines = self.parse_program_lines(lines);
+        println!("{:#?}", pr_lines);
     }
 
-    pub fn parse(&self, lines: Vec<String>) {
-        let symbols = SymbolTable::new();
-        let tags = Tags::new();
+    pub fn parse_program_lines(&self, lines: Vec<String>) -> Vec<ProgramLine> {
+        let mut result = Vec::new();
 
         for line in lines {
             if line.starts_with("*") {
@@ -65,17 +122,26 @@ impl Parser {
             let mut address = String::new();
             let pseudo_op = new_if_presudo_of(op, line);
             if pseudo_op.is_none() {
-                if !tags.is_instruction(op) {
+                if !self.tags.is_instruction(op) {
                     panic!("unsupported operation {op}");
                 }
                 let (left, _) = split_whitespace_once(line);
                 address = left.to_string();
             } else {
-                address = pseudo_op.expect("err").parse_address();
+                let pseudo_op = pseudo_op.expect("err");
+                address = pseudo_op.parse_address();
+
+                // match pseodo_op {
+                    // EQU => self.defined_symbols.put(loc, address),
+                // }
             }
 
-            println!("{loc}|{op}|{address}|");
+            let pr_line = ProgramLine::new(loc.to_string(), op.to_string(), address.to_string());
+            result.push(pr_line);
+            // println!("{:#?}", pr_line);
         }
+
+        result
     }
 }
 
---
+++ mixal/src/parser/word.rs
@@ -0,0 +1,5 @@
+pub struct Word {
+}
+
+pub struct WValue {
+}
